8324f299eacabfa63f90596b4f42b7aebf553217
Iqra-Bakhtiyaar <34320259+Iqra-Bakhtiyaar@users.noreply.github.com>
2018-03-08 12:22:14 +0000

    Add files via upload

---
+++ Beu.php
@@ -0,0 +1,393 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>B€YøU</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css" type="text/css";>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+  <!-- scroll down body type  -->
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+  <!-- Navigation bar -->
+<nav class="navbar navbar-inverse">
+<div class="container-fluid">
+<div class="navbar-header">
+<a class="navbar-brand" href=#> B€YøU</a>
+</div>
+<ul class="nav navbar-nav navbar-right">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#login">Login</a></li>
+       <li><a href="#register">Sign up</a></li>
+      <li><a href="#women">Women</a></li>
+      <li><a href="#men">Men</a></li>
+       <li><a href="#">
+          <span class="glyphicon glyphicon-shopping-cart"></span> Shopping cart
+        </a>
+    </ul>
+  </div>
+</nav>
+  <!-- Carousal pointer and slider-->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+    <!-- Images for carousel-->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/81099033_12-99999999_01.jpg" alt="Dress">
+          <!-- Add caption -->
+                <div class="carousel-caption">
+          <p> Be Gorgeous </p>
+          <p> Be Yourself </p>
+          <h1> B€YøU </h1>
+          </div>
+    </div>
+
+    <div class="item">
+          <img src="images/83099009_01.jpg" alt="Shirt">
+    </div>
+
+     <div class="item">
+          <img src="images/Jacket.jpg" alt="Jacket">
+    </div>
+    </div>
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  </div>
+
+<!-- A jumbotron container-->
+  <div class="container">
+  <div class="jumbotron">
+    <h1>Our vision</h1>      
+    <p>We created this brand for people who want to be themselves instead of following latest fashion trends.</p>
+     <p>You will find here every kind of designs matching your personality. So pick as much as you like and B€YøU </p>     
+</div>
+</div>
+
+<!-- Women -->
+<div id="women" class="bg-1">
+<div class="container">
+<h1> Women </h1>
+<!-- Create row class for products-->
+<div class="row">
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/13070563_50-99999999_01.jpg" alt="women" width="350" height="500">
+<!-- Create product caption-->
+          <div class="caption">
+            <p>Blue shirt <br>£9.99</p>
+          </div>
+</div>
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/81038814_05-99999999_01.jpg" alt="women" width="350" height="500">
+<!-- Create product caption-->
+<div class="caption">
+            <p> Cut sleeves top <br>£10.99</p>
+          </div>
+</div>
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/81047611_12-99999999_01.jpg" alt="women" width="350" height="500">
+<!-- Create product caption-->
+<div class="caption">
+            <p> Yellow ruffle blouse <br>£15.99</p>
+          </div>
+</div>
+</div>
+
+<!-- Create another row class for products-->
+<div class="row">
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/81068823_52-99999999_01.jpg" alt="women" width="350" height="500">
+<!-- Create product caption-->
+<div class="caption">
+            <p> Sleeveless top <br>£10.99</p>
+          </div>
+</div>
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/83089006_01-99999999_01.jpg" alt="women" width="350" height="500">
+<!-- Create product caption-->
+<div class="caption">
+            <p> White top <br>£12.99</p>
+          </div>
+</div>
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/83099009_01-99999999_01.jpg" alt="women" width="350" height="500">
+<!-- Create product caption-->
+<div class="caption">
+            <p> White lace top <br>£10.99</p>
+          </div>
+</div>
+</div>
+
+
+<div class="row">
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/83079022_01-99999999_01.jpg" alt="women" width="350" height="500">
+<!-- Create product caption-->
+<div class="caption">
+            <p> White top <br>£7.99</p>
+          </div>
+</div>
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/81038825_70_B.jpg" alt="women" width="350" height="500">
+<!-- Create product caption-->
+<div class="caption">
+            <p> Orange top <br>£10.99</p>
+          </div>
+</div>
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/83089038_99-99999999_01.jpg" alt="women" width="350" height="500">
+<!-- Create product caption-->
+<div class="caption">
+            <p> Rolly poly top <br>£18.99</p>
+          </div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+<!-- Men -->
+<div id="men" class="bg-1">
+<div class="container">
+<h1> Men </h1>
+<!-- Create row class for products-->
+<div class="row">
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/13020270_01.jpg" alt="White shirt" width="350" height="500">
+<!-- Create product caption-->
+          <div class="caption">
+            <p>White shirt <br>£9.99</p>
+          </div>
+</div>
+
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/13070285_56_B.jpg" alt="Little flower men shirt" width="350" height="500">
+<!-- Create product caption-->
+          <div class="caption">
+            <p>Little flower men shirt <br>£10.99</p>
+          </div>
+</div>
+
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/13080264_36.jpg" alt="Light Green shirt" width="350" height="500">
+<!-- Create product caption-->
+          <div class="caption">
+            <p>Light Green shirt <br>£15.99</p>
+          </div>
+</div>
+
+<!-- Create another row class for products-->
+<div class="row">
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/83019000_15.jpg" alt="Yellow shirt" width="350" height="500">
+<!-- Create product caption-->
+          <div class="caption">
+            <p>Yellow shirt <br>£11.99</p>
+          </div>
+</div>
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/83019000_45.jpg" alt="Sky blue shirt" width="350" height="500">
+<!-- Create product caption-->
+          <div class="caption">
+            <p>Sky blue shirt and jeans <br>£13.99</p>
+          </div>
+</div>
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/83053507_56.jpg" alt="Men suit" width="350" height="500">
+<!-- Create product caption-->
+          <div class="caption">
+            <p>Men suit<br>£99.99</p>
+          </div>
+</div>
+
+
+
+
+<!-- Create another row class for products-->
+<div class="row">
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/83060026_51.jpg" alt="Men suit" width="350" height="500">
+<!-- Create product caption-->
+          <div class="caption">
+            <p>Men suit blue<br>£125.99</p>
+          </div>
+</div>
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/83090122_60.jpg" alt="Men suit" width="350" height="500">
+<!-- Create product caption-->
+          <div class="caption">
+            <p>Men suit jacket <br>£50.99</p>
+          </div>
+</div>
+
+<div class="col-md-4">
+<!-- Insert image-->
+<img class="img-responsive" src="images/83093506_51.jpg" alt="Full men suit" width="350" height="500">
+<!-- Create product caption-->
+          <div class="caption">
+            <p>Full men suit<br>£99.99</p>
+          </div>
+          </div>
+          </div>
+
+<!-- Create login -->
+<div class="container" id="login">
+<div class="jumbotron" style="margin-top: 5%;">
+  <h1>Login</h1>
+  </div>
+
+  <form class="form-horizontal ">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Email:</label>
+      <div class="col-sm-8">
+        <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email here">
+      </div>
+    </div>
+
+      <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Password:</label>
+      <div class="col-sm-8">          
+        <input type="password" name="pwd" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+      </div>
+    </div>
+
+  <div class="form-group">        
+      <div class="col-sm-8">
+        <div class="checkbox">
+          <label><input type="checkbox" name="remember"> Remember me</label>
+        </div>
+      </div>
+    </div>
+
+    <div class="form-group">        
+      <div class="col-sm-8">
+        <button type="submit" class="btn btn-default">Login</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+
+
+
+
+<!-- Register -->
+<div class="container" id="register">
+<div class="jumbotron" id="registerj" style="margin-top: 5%;"">
+  <h1>Sign up</h1>
+  </div>
+
+
+    <form class="form-horizontal">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="firstname">First Name:</label>
+      <div class="col-sm-8">
+        <input type="text" name="firstname" class="form-control" id="firstName" placeholder="Enter your first name">
+      </div>
+    </div>
+
+
+        <form class="form-horizontal ">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="lastname">Last Name:</label>
+      <div class="col-sm-8">
+        <input type="text" name="lastname" class="form-control" id="lastname" placeholder="Enter your last name">
+      </div>
+    </div>
+
+
+            <form class="form-horizontal ">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="age">Age:</label>
+      <div class="col-sm-8">
+        <input type="text" name="age" class="form-control" id="age" placeholder="Enter your age here">
+      </div>
+    </div>
+
+            <form class="form-horizontal ">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="gender">Gender:</label>
+      <div class="col-sm-8">
+        <input type="text" name="gender" class="form-control" id="gender" placeholder="Enter your gender here">
+      </div>
+    </div>
+
+  <form class="form-horizontal ">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Email:</label>
+      <div class="col-sm-8">
+        <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email here">
+      </div>
+    </div>
+
+      <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Password:</label>
+      <div class="col-sm-8">          
+        <input type="password" name="pwd" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+      </div>
+    </div>
+
+
+
+    <div class="form-group">        
+      <div class="col-sm-8">
+        <button type="submit" class="btn btn-default">Sign up</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+
+
+
+		
+
+
